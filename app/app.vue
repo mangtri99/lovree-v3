e86a0bd42d75335bf0817eaef014b2c3220a421b
Mang Tri <mangtri93@gmail.com>
2026-06-13 06:46:07 +0800

    feat: load curated Google Fonts globally so token fonts actually render

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { googleFontsHref } from '../server/theme/fonts'
+
+useHead({
+  link: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+    { rel: 'stylesheet', href: googleFontsHref() },
+  ],
+})
+</script>
+
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
