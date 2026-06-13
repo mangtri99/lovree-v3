73abfdac41de8d982d7bc82c7fb97b32a6498343
Mang Tri <mangtri93@gmail.com>
2026-06-14 06:28:01 +0800

    feat: GET /api/admin/themes lists available themes

---
+++ server/api/admin/themes.get.ts
@@ -0,0 +1,9 @@
+import { useDb } from '../../db'
+import { themes } from '../../db/schema'
+
+export default defineEventHandler(async (event) => {
+  const session = await getUserSession(event)
+  if (!session.user?.id) throw createError({ statusCode: 401, message: 'Unauthorized' })
+  const db = useDb()
+  return await db.select({ id: themes.id, name: themes.name, tokens: themes.tokens, previewImage: themes.previewImage }).from(themes)
+})
